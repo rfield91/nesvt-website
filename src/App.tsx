d6adf1546e4c31f09fab91ae4ab5f707fb849a2f
Ryan Field <navalhawkeye@gmail.com>
2025-11-03 12:18:13 -0500

    Placeholder ne-svt website

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='h-dvh flex flex-col items-center justify-center bg-black gap-2'>
+      <div className='text-orange-500 text-3xl'>
+        NE-SVT
+      </div>
+      <div className="tenor-gif-embed md:max-w-1/2" data-postid="26018511" data-share-method="host" data-aspect-ratio="1.46789" data-width="100%">
+        <a href="https://tenor.com/view/hellmo-pizza-fire-creed-hellmo-pizza-fire-gif-26018511">Hellmo Pizza GIF</a>
+        from 
+        <a href="https://tenor.com/search/hellmo-gifs">Hellmo GIFs</a>
+      </div>
+      <div className='text-orange-500 text-3xl'>
+        Has Pizza
+      </div>
+      <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+    </div>
+  )
+}
+
+export default App
